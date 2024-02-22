f30cf682aaaf88345eb692d8bda9a4e8dc40d31d
Teja2121 <56876637+Teja2121@users.noreply.github.com>
2024-02-21 22:15:42 -0500

    part_3_template_solution.py

--- part_3_template_solution.py
+++ part_3_template_solution.py
@@ -88,13 +88,10 @@ class Section3:
 
         answer = {}
 
-        print("Part 3(A) - \n")
+
         Xtrain, ytrain, Xtest, ytest = u.prepare_data()
 
-        # Initialize the ShuffleSplit cross-validator
-        #shuffle_split = ShuffleSplit(n_splits=5, random_state=42)
 
-        # Initialize and train the classifier
         clf_log = LogisticRegression(max_iter=300, random_state=42)
         clf_log.fit(Xtrain,ytrain)
 
@@ -115,47 +112,12 @@ class Section3:
             # Store the scores in the answer dictionary for each k
             answer[k] = {"score_train": score_train_k, "score_test": score_test_k}
 
-        """
-        # Plotting
-        plt.plot(k_values, train_scores, label='Training Data')
-        plt.plot(k_values, test_scores, label='Testing Data')
-        plt.xlabel('Top k')
-        plt.ylabel('Accuracy Score')
-        plt.title('Top-k Accuracy Scores for Training and Testing Sets')
-        plt.legend()
-        plt.show()
-        """
-
-        #print("Training scores:", train_scores)
-        #print("Testing scores:", test_scores)
-
         
         answer["clf"] = LogisticRegression(max_iter=300, random_state=42)
         answer["plot_k_vs_score_train"] = list(zip(k_values ,train_scores))
         answer["plot_k_vs_score_test"] = list(zip(k_values, test_scores))
         answer["text_rate_accuracy_change"] = "The rate of accuracy for testing data, increased with increase in the value of k"
         answer["text_is_topk_useful_and_why"] = "Yes, topk is useful because it measures the accuracy of a classifier's predictions when considering the top k predicted classes instead of just the most probable one."
-
-        print(answer)
-        """
-        # `answer` is a dictionary with the following keys:
-        - integers for each topk (1,2,3,4,5)
-        - "clf" : the classifier
-        - "plot_k_vs_score_train" : the plot of k vs. score for the training data, 
-                                    a list of tuples (k, score) for k=1,2,3,4,5
-        - "plot_k_vs_score_test" : the plot of k vs. score for the testing data
-                                    a list of tuples (k, score) for k=1,2,3,4,5
-
-        # Comment on the rate of accuracy change for testing data
-        - "text_rate_accuracy_change" : the rate of accuracy change for the testing data
-
-        # Comment on the rate of accuracy change
-        - "text_is_topk_useful_and_why" : provide a description as a string
-
-        answer[k] (k=1,2,3,4,5) is a dictionary with the following keys: 
-        - "score_train" : the topk accuracy score for the training set
-        - "score_test" : the topk accuracy score for the testing set
-        """
 
         return answer, Xtrain, ytrain, Xtest, ytest
 
@@ -180,7 +142,6 @@ class Section3:
         """"""
         # Enter your code and fill the `answer` dictionary
         answer = {}
-        print("Part 3(B) - \n")
         X, y, Xtest, ytest = u.prepare_data()
         Xtrain, ytrain = nu.filter_imbalanced_7_9s(X, y)
         Xtest, ytest = nu.filter_imbalanced_7_9s(Xtest, ytest)
@@ -189,10 +150,6 @@ class Section3:
         # Checking that the labels are integers
         ytrain_test = nu.scale_data_1(ytrain)
         ytest_test = nu.scale_data_1(ytest)
-        print("3(B) - Are elements in Xtrain a floating point number and scaled between 0 to 1: " +str(Xtrain_test))
-        print("3(B) - Are elements in a floating point number and scaled between 0 to 1: " +str(Xtest_test))
-        print("3(B) - Are elements in ytrian an integer: " +str(ytrain_test))
-        print("3(B) - Are elements in ytest an integer: " +str(ytest_test))
         answer = {}
 
         length_Xtrain1 = len(Xtrain)
@@ -201,8 +158,6 @@ class Section3:
         length_ytest1 = len(ytest)
         max_Xtrain1 = Xtrain.max()
         max_Xtest1 = Xtest.max()
-        print(f"3(B) - Length of Xtrain, Xtest, ytrain, ytest is: {length_Xtrain1}, {length_Xtest1}, {length_ytrain1}, {length_ytest1}")
-        print(f"3(B) - Max value of Xtrain and Xtest is: {max_Xtrain1}, {max_Xtest1}")
         answer["length_Xtrain"] = length_Xtrain1  # Number of samples
         answer["length_Xtest"] = length_Xtest1
         answer["length_ytrain"] = length_ytrain1
@@ -234,18 +189,14 @@ class Section3:
     ) -> dict[str, Any]:
         """"""
         # Part 3(c)
-        print("Part 3(C) - \n")
+
 
         def stratified_kfold():
             return StratifiedKFold(n_splits=5, shuffle=True, random_state=42)
         
-        # Define a function to train a classifier with cross-validation
         def train_classifier_with_cv(Xtrain, ytrain, clf):
-        # Define scoring metrics
             scoring = {'accuracy': 'accuracy', 'f1_score': make_scorer(f1_score, average='macro'), 'precision': make_scorer(precision_score, average='macro'),'recall': make_scorer(recall_score, average='macro')}
-        # Perform cross-validation
             scores = cross_validate(clf, Xtrain, ytrain, cv=stratified_kfold(), scoring=scoring)
-        # Print the mean and std of scores
             u.print_cv_result_dict(scores)
             return scores
 
@@ -253,7 +204,6 @@ class Section3:
         Xtrain, ytrain = nu.filter_imbalanced_7_9s(X, y)
         Xtest, ytest = nu.filter_imbalanced_7_9s(Xtest, ytest)
 
-        # Train SVC with cross-validation
         clf_svc = SVC(random_state=42)
         scores_svc = train_classifier_with_cv(Xtrain, ytrain, clf_svc)
 
@@ -274,23 +224,12 @@ class Section3:
 
         pres_high_recall = score_values_svc1["mean_precision"] > score_values_svc1["mean_recall"]
         
-        
-        #print(score_values_svc1)
-
-        # Train the classifier on all training data
         clf_svc.fit(Xtrain, ytrain)
 
-        # Plot confusion matrix for the test data
         y_pred_svc_train = clf_svc.predict(Xtrain)
         y_pred_svc_test = clf_svc.predict(Xtest)
         cm_svc_train = confusion_matrix(ytrain, y_pred_svc_train)
         cm_svc_test = confusion_matrix(ytest, y_pred_svc_test)
-        print("Confusion matrix for training data: \n")
-        print(cm_svc_train)
-        print("Confusion matrix for testing data:")
-        print(cm_svc_test)
-
-        
 
         # Enter your code and fill the `answer` dictionary
         answer = {}
@@ -340,7 +279,6 @@ class Section3:
         """"""
         # Enter your code and fill the `answer` dictionary
         answer = {}
-        print("Part 3(D) - \n")
 
         def stratified_kfold():
             return StratifiedKFold(n_splits=5, shuffle=True, random_state=42)
@@ -358,33 +296,18 @@ class Section3:
             u.print_cv_result_dict(scores)
             return scores, class_weight_dict
 
-        
-
         X, y, Xtest, ytest = u.prepare_data()
         Xtrain, ytrain = nu.filter_imbalanced_7_9s(X, y)
         Xtest, ytest = nu.filter_imbalanced_7_9s(Xtest, ytest)
     
-        # Train SVC with cross-validation using weighted loss function
         clf_svc_weighted = SVC(random_state=42)
         scores_svc_weighted, class_weight_dict_outside = train_classifier_with_weighted_cv(Xtrain, ytrain, clf_svc_weighted)
-
-        
-        print("The weights are: \n")
-        print(class_weight_dict_outside)
-        
-
-        # Train the classifier on all training data
         clf_svc_weighted.fit(Xtrain, ytrain)
 
-        # Plot confusion matrix for the test data
         y_pred_svc_train1 = clf_svc_weighted.predict(Xtrain)
         y_pred_svc_test1 = clf_svc_weighted.predict(Xtest)
         cm_svc_train1 = confusion_matrix(ytrain, y_pred_svc_train1)
         cm_svc_test1 = confusion_matrix(ytest, y_pred_svc_test1)
-        print("Confusion matrix for training data: \n")
-        print(cm_svc_train1)
-        print("Confusion matrix for testing data:")
-        print(cm_svc_test1)
 
         scores_dict = scores_svc_weighted
 
@@ -412,7 +335,6 @@ class Section3:
         answer["explain_purpose_of_class_weights"] = "Class weights are used to address class imbalance in classification problems"
         answer["explain_performance_difference"] = "Using class weights in the SVM classifier results in improved overall accuracy, F1 score, and recall, indicating better performance, particularly in correctly identifying positive instances. However, there's a slight decrease in precision when using class weights. This trade-off suggests that while class weights help in better identifying minority class instances, there may be a slight increase in false positives."
 
-        print(answer)
         """
         Answer is a dictionary with the following keys: 
         - "scores" : a dictionary with the mean/std of the F1 score, precision, and recall
